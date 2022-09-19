003eb2615d6ca630cb1c09f2d6df2e67ac57130a
Annalena Kalteyer <akalteye@lxir128.gsi.de>
2022-09-19 10:51:35 +0200

    change path

--- Tools/KFparticle/qaKFParticle.cxx
+++ Tools/KFparticle/qaKFParticle.cxx
@@ -32,11 +32,11 @@
 #define HomogeneousField
 #endif
 
-// #include "KFParticle/KFPTrack.h"
-// #include "KFParticle/KFPVertex.h"
-#include <KFParticle.h>
-// #include "KFParticle/KFParticleBase.h"
-// #include "KFParticle/KFVertex.h"
+#include <KFParticle/KFParticle.h>
+#include "KFParticle/KFPTrack.h"
+#include "KFParticle/KFPVertex.h"
+#include "KFParticle/KFParticleBase.h"
+#include "KFParticle/KFVertex.h"
 
 using namespace o2;
 using namespace o2::framework;
